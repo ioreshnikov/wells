bea43ce35503efb9bc6318d4512258080fa0e9f7
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2016-07-24 22:23:01 +0300

    p -> pump, kappa -> loss.

--- track_mode_complex.sh
+++ track_mode_complex.sh
@@ -4,8 +4,8 @@
 LANG=C;
 
 
-p=0.05;
-kappa=0.001;
+pump=0.05;
+loss=0.001;
 
 
 mode=4;
@@ -21,7 +21,7 @@ deltas=`seq $start $step $stop`;
 
 # output="Conservative/mode=${mode}_delta=${start}.npz";
 for delta in $deltas; do
-    command="./ztqho.py --p $p --kappa $kappa --n $mode --delta $delta --scale $scale --phase $phase --label $label";
+    command="./ztqho.py --pump $pump --loss $loss --n $mode --delta $delta --scale $scale --phase $phase --label $label";
     if [ -n "$output" ]; then
         command="$command --input $output";
     fi

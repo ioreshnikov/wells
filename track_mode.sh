ff7f682c540220900dad93be70e44a2e05f7b47d
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2016-08-22 02:55:22 +0300

    Better resolution in tqho.py

--- track_mode.sh
+++ track_mode.sh
@@ -4,15 +4,16 @@
 LANG=C;
 
 
-n=6;
+n=0;
 
-min=-6.5;
+min=-0.5;
 max=+5.0;
 step=0.1;
 deltas=`seq $min $step $max`;
 
 
 for delta in $deltas; do
+    echo $delta;
     command="./tqho.py --n $n --delta $delta";
     if [ -n "$output" ]; then
         command="$command --input $output";

5e0e36b390961b0591a5056d2ee8ffa2d3fc9d6f
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2016-08-27 16:51:31 +0300

    Sync.

--- track_mode_complex.sh
+++ track_mode_complex.sh
@@ -9,19 +9,18 @@ pump=0.500;
 loss=0.001;
 
 
-mode=2;
+mode=0;
 label="$mode-";
-scale=1.00;
+scale=0.90;
 phase=0.00;
 
-start=0.00;
-stop=5.00;
-step=0.10;
+start=5.00;
+stop=-0.50;
+step=-0.10;
 deltas=`seq $start $step $stop`;
 
 
-output="Conservative/mode=${mode}_delta=${start}.npz";
-output="mode=2_delta=0.00_pump=5.00E-01_loss=1.00E-03_2-.npz";
+output="Conservative128/mode=${mode}_delta=${start}.npz";
 for delta in $deltas; do
     command="./ztqho.py --pump $pump --loss $loss --n $mode --delta $delta --scale $scale --phase $phase --label $label";
     if [ -n "$output" ]; then
